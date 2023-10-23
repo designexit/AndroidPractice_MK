476b0d35fc48911d9b4d8ecbdd7e2505a5644610
“designexit” <didmk.lab@gmail.com>
2023-10-23 22:57:02 +0900

    인트로, 메인, 로그인 ,회원가입

--- myapplication_test5/src/main/java/com/example/myapplication_test5/IntroActivity.kt
+++ myapplication_test5/src/main/java/com/example/myapplication_test5/IntroActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
         val handler = Handler()
         handler.postDelayed(Runnable {
             val intent = Intent(applicationContext, MainActivity::class.java)
@@ -17,10 +16,8 @@ class IntroActivity : AppCompatActivity() {
             finish()
         }, 3000) // 3초 있다 메인액티비티로
 
+        setContentView(R.layout.activity_intro)
     }
 
-    override fun onPause() {
-        super.onPause()
-        finish()
-    }
+
 }
--- myapplication_test5/src/main/java/com/example/myapplication_test5/JoinActivity.kt
+++ myapplication_test5/src/main/java/com/example/myapplication_test5/JoinActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication_test5
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication_test5.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityJoinBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+
+        binding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
